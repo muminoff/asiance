fc2bf047f636db9bdbe098d40742bcb0fb1ae660
muminoff <smuminov@gmail.com>
2019-08-13 22:56:29 +0900

    Update header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">The Blog</h1>
       </header>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
