f38284b05d7651faae3cad76a9654f25f323d485
muminoff <smuminov@gmail.com>
2019-08-13 22:04:35 +0900

    Add new model `Author`

--- asiance/api/models.py
+++ asiance/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+    avatar = models.ImageField(upload_to="images", blank=True, null=True)
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.role})"
