3130e7981e56933e0128704887f85ea5c517ee1a
muminoff <smuminov@gmail.com>
2019-08-13 22:50:43 +0900

    Get rid of React logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './App.css';
 import './Font.css';
@@ -8,7 +7,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
       <p className="App-intro">
