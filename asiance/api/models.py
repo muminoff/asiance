aac9839ed1091cdb72668fc7c102fe9001c20b93
muminoff <smuminov@gmail.com>
2019-08-13 22:08:34 +0900

    Add new model

--- asiance/api/models.py
+++ asiance/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils.text import Truncator
 import uuid
 
 
@@ -13,3 +15,21 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.role})"
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    body = models.TextField(null=True, blank=True)
+    tags = ArrayField(models.CharField(max_length=200), blank=True)
+    image_url = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.title:
+            title_trunc = Truncator(self.title).words(8)
+            return f"{title_trunc} by {self.author.name}"
+
+        return self.pk
