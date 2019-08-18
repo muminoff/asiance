911d65a5a0fab5756a89efc17eedfbc6645ca3dc
muminoff <smuminov@gmail.com>
2019-08-18 23:21:33 +0900

    New changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
-    avatar = models.ImageField(upload_to="images", blank=True, null=True)
+    avatar = models.URLField(null=True)
     location = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
